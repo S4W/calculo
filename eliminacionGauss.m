cd4142c9db65330535965a51f702fc8547351bcd
Rafael Cisneros <rafucho_A_cisneros@hotmail.com>
2017-10-05 22:34:23 -0400

    arreglado gauss con pivoteo

--- eliminacionGauss.m
+++ eliminacionGauss.m
@@ -3,25 +3,34 @@ function C = eliminacionGauss(A,b)
 	n = dimensionA(1)
 	C = crearMatrizExtendida(A, b)
 
-	for k = 2 : 1 : n
-		i0 = k-1
-		for i = k : 1 : n
-			if abs(C(i0,k-1) < abs(C(i,k-1)))
+	for k = 1 : 1 : n-1
+		i0 = k
+		for i = k+1 : 1 : n
+			if abs(C(i0,k)) < abs(C(i,k))
 				i0 = i
 			end
 		end
-		for j = 1 : 1 : n+1
+		for j = k : 1 : n
 			S = C(i0,j)
-			C(i0,j) = C(k-1,j)
-			C(k-1,j) = S
+			C(i0,j) = C(k,j)
+			C(k,j) = S
 		end
-		for i = k : 1 : n
-			alpha = C(i,k-1) / C(k-1, k-1)
-			for j = k : 1 : n+1
-				C(i,j) = C(i,j) - alpha * C(k-1,j)
+
+		S = C(i0,n+1)
+		C(i0,n+1) = C(k,n+1)
+		C(k,n+1) = S
+		
+		for i = k+1 : 1 : n
+			alpha = C(i,k) / C(k, k)
+			for j = k : 1 : n
+				C(i,j) = C(i,j) - alpha * C(k,j)
 			end
+			C(i,n+1) = C(i,n+1) - alpha * C(k,n+1)
 		end
 	end
+
+
+	
 function matrizExtendida = crearMatrizExtendida(A, b)
 	dimensionA = size(A)
 
@@ -34,3 +43,4 @@ function matrizExtendida = crearMatrizExtendida(A, b)
 	for i = 1: 1 : dimensionA(1)
 		matrizExtendida(i, dimensionA(1)+1) = b(i)
 	end
+	echo off

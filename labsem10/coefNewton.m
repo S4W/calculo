d365aa0ae1ee489321eb4c4183cd27119b205515
Rafael Cisneros <rafucho_A_cisneros@hotmail.com>
2017-11-23 21:51:20 -0400

    newton

--- labsem10/coefNewton.m
+++ labsem10/coefNewton.m
@@ -1,17 +1,22 @@
-function coefNewton = coefNewton(Xs,Ys)
+function [dif,c] = coefNewton(Xs,Ys)
     n = size(Xs);
     n = n(2);
 
     for i = 1:1:n
-        c(i) = Ys(i)
-        dif(i,1) = Ys(i)
+        c(i) = Ys(i);
+        dif(i,1) = Ys(i);
     end
 
+    for j=2:n
+        for i=1:n-j+1
+            dif(i,j) = (dif(i+1,j-1)-dif(i,j-1))/(Xs(i+j-1)-Xs(i));
+        end
+    end
 
 
     for j = 2:1:n
         for i = n:-1:j
-            c(i) = (c(i)-c(i-1))/(x(u)-x(i-j+1))
+            c(i) = (c(i)-c(i-1))/(Xs(i)-Xs(i-j+1));
         end
     end
 
